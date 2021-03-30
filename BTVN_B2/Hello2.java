e75a4118c48993f8a2aec26dd4096ecce60f4cd3
Do <dophamutc@gmail.com>
2021-03-30 21:33:27 +0700

    Test git 5

--- BTVN_B2/Hello2.java
+++ BTVN_B2/Hello2.java
@@ -2,6 +2,6 @@ package BTVN_B2;
 
 public class Hello2 {
     public static void main(String[] args) {
-        System.out.println("test git 4");
+        System.out.println("test git 5");
     }
 }
